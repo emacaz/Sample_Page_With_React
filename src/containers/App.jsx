ba51bebe74fe034767504ded137b36b9e118ba66
Emanuel Castillo <ecastillo76@misena.edu.co>
2020-06-23 18:03:26 +0200

    Update no. 18

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
-import Header from '../components/Header';
+import React from 'react'
+import Header from '../components/Header'
 import Search from '../components/Search'
-import '../assets/styles/App.scss';
+import Categories from '../components/Categories'
+import Carousel from '../components/Carousel'
+import CarouselItem from '../components/CarouselItem'
+import '../assets/styles/App.scss'
 
 const App = () => (
   <div className="App">
     <Header />
     <Search />
-  </div>
-);
 
-export default App;
+    <Categories>
+      <Carousel>
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+        <CarouselItem />
+      </Carousel>
+    </Categories>
+
+  </div>
+)
+
+export default App
