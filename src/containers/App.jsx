5db8e85febc73a49eed714276c9da66c066d3bc5
Emanuel Castillo <ecastillo76@misena.edu.co>
2020-06-25 00:26:37 +0200

    Update no. 23

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from '../components/Header'
 import Search from '../components/Search'
 import Categories from '../components/Categories'
@@ -7,36 +7,49 @@ import CarouselItem from '../components/CarouselItem'
 import Footer from '../components/Footer'
 import '../assets/styles/App.scss'
 
-const App = () => (
-  <div className="App">
-    <Header />
-    <Search />
+const App = () => {
 
-    <Categories title="Mi Lista">
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
-    
-    <Categories title="Tendencias">
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
-    
-    <Categories title="Originales">
-      <Carousel>
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+  const [ videos, setVideos ] = useState([])
 
-    <Footer />
+  useEffect(() => {
+    fetch('http://localhost:3000/initialState')
+      .then(response => response.json())
+      .then(data => setVideos(data))
+  }, [])
 
-  </div>
-)
+  console.log(videos)
+  
+
+  return (
+    <div className="App">
+      <Header />
+      <Search />
+
+      <Categories title="Mi Lista">
+        <Carousel>
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+          <CarouselItem />
+        </Carousel>
+      </Categories>
+      
+      <Categories title="Tendencias">
+        <Carousel>
+          <CarouselItem />
+          <CarouselItem />
+        </Carousel>
+      </Categories>
+      
+      <Categories title="Originales">
+        <Carousel>
+          <CarouselItem />
+        </Carousel>
+      </Categories>
+
+      <Footer />
+
+    </div>
+)}
 
 export default App
