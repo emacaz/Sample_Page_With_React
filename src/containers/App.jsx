4b6af6dff45795339e04699a33a0dcc3e4928ad8
Emanuel Castillo <ecastillo76@misena.edu.co>
2020-06-25 17:39:22 +0200

    Finishig website sample in ReactNative

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -5,45 +5,44 @@ import Categories from '../components/Categories'
 import Carousel from '../components/Carousel'
 import CarouselItem from '../components/CarouselItem'
 import Footer from '../components/Footer'
+import useInitialState from '../hooks/useInitialState'
 import '../assets/styles/App.scss'
+
+const API = 'http://localhost:3000/initialState/'
 
 const App = () => {
 
-  const [ videos, setVideos ] = useState([])
+  const initialState = useInitialState(API)
 
-  useEffect(() => {
-    fetch('http://localhost:3000/initialState')
-      .then(response => response.json())
-      .then(data => setVideos(data))
-  }, [])
-
-  console.log(videos)
-  
-
-  return (
+  return initialState.length === 0 ? <h1>Loading...</h1> : (
     <div className="App">
       <Header />
       <Search />
 
-      <Categories title="Mi Lista">
-        <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-        </Carousel>
-      </Categories>
+      {
+        initialState.mylist.length > 0 &&
+          <Categories title="Mi Lista">
+            <Carousel>
+              <CarouselItem />
+            </Carousel>
+          </Categories>
+      }
       
       <Categories title="Tendencias">
         <Carousel>
-          <CarouselItem />
-          <CarouselItem />
+          {
+            initialState.trends.map(item => 
+              <CarouselItem key={item.id} {...item}/>
+          )}
         </Carousel>
       </Categories>
       
       <Categories title="Originales">
         <Carousel>
-          <CarouselItem />
+          {
+            initialState.originals.map(item =>
+              <CarouselItem key={item.id} {...item}/>
+          )}
         </Carousel>
       </Categories>
 
