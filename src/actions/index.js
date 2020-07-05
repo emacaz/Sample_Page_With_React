9aeddd2d09457c88f6d2695e58155245ef26dca8
Emanuel Castillo <ecastillo76@misena.edu.co>
2020-07-05 02:40:46 +0200

    Update no. 20 - LoginLogout validation code

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,3 +12,10 @@ export const loginRequest = payload => ({
   type: 'LOGIN_REQUEST',
   payload
 })
+
+export const logoutRequest = payload => (
+  {
+    type: 'LOGOUT_REQUEST',
+    payload
+  }
+)
